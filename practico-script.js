b658ec0c0976ada0815e522e04839ef29d479d4b
Ram-Gad <mg.rafaeld@gmail.com>
2024-01-17 12:49:52 -0300

    terminada clase de fusion del menu mobile, falta practicar

--- practico-script.js
+++ practico-script.js
@@ -1,9 +1,16 @@
 const navbarEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const burguerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
+
+burguerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
 }
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
