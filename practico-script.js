5c3b074a9a3bd9256c6953e60b89200e9dcda663
Ram-Gad <mg.rafaeld@gmail.com>
2024-01-25 12:58:58 -0300

    clase de creacion de elementos dentro de arrays casi terminada

--- practico-script.js
+++ practico-script.js
@@ -4,8 +4,11 @@ const burguerMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const shoppingCart = document.querySelector('.navbar-shopping-cart')
 const asideCartMenu = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
+const mainContainer = document.querySelector('.main-container')
 
 
+/////////////////////////////////////////////////////////////////////
 navbarEmail.addEventListener('click', toggleDeskopMobileMenuClass)
 
 burguerMenu.addEventListener('click', toggleDeskopMobileMenuClass)
@@ -73,3 +76,93 @@ function aparecerPanel2 () {
         panel1.classList.add('inactive')
     }
 }*/
+
+//Cards container
+
+/* 
+<div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>
+*/
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Mouse',
+    price: 10,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Laptop',
+    price: 720,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Parlante',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Guitarra',
+    price: 400,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Amplificador',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image)
+    productImg.setAttribute('alt', 'imagen del producto')
+
+    productCard.appendChild(productImg)
+
+    const productInfo = document.createElement('div')
+    productInfo.classList.add('product-info');
+
+
+    const detailedInfo = document.createElement('div')
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText ='$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    detailedInfo.append(productPrice, productName)
+
+    const productFigure = document.createElement('figure')
+
+    const addToCartImg = document.createElement('img')
+    addToCartImg.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+    productFigure.appendChild(addToCartImg)
+
+    productInfo.appendChild(detailedInfo)
+    productInfo.appendChild(productFigure)
+
+    productCard.appendChild(productImg)
+    productCard.appendChild(productInfo)
+
+    cardsContainer.appendChild(productCard)
+}
